5cabd8020f7fbac9452293112dbfd273f841a5fc
unknown <premrnjan@gmail.com>
2024-06-05 21:59:35 +0530

    first app hello world

---
+++ Awesome01/App.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import {
+  Text,
+  View,
+  SafeAreaView
+} from 'react-native'
+
+function App(){
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>Hellow World !</Text>
+    </View>
+  </SafeAreaView>
+  )
+}
+
+export default App;
