9455dc6a8c0d3f37283ec9f236550c832eab7f48
unknown <premrnjan@gmail.com>
2024-06-08 16:53:20 +0530

    Add Elevated Cards & Fancy Cards

--- Styling/App.tsx
+++ Styling/App.tsx
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import {SafeAreaView, ScrollView, Text, View} from 'react-native';
 import FlatCards from './FlatCards';
+import ElevatedCards from './ElevatedCards';
+import FancyCard from './FancyCards';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <FlatCards></FlatCards>
-      </View>
+      <SafeAreaView>
+        <ScrollView>
+          <FlatCards></FlatCards>
+          <ElevatedCards></ElevatedCards>
+          <ScrollView>
+            <FancyCard/>
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
